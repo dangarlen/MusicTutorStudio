32664ada9405857ae88e0d89edfccf37b7877aa8
Dan Garlen <dan.g@kgsystems.com>
2025-10-25 09:55:24 -0400

    alpha-vue/index looks same as alpha/index after CSS rebuid

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/alpha-vue/**/*.vue", "./public/alpha-vue/**/*.js"],
+  content: [
+    "./public/alpha-vue/**/*.vue",
+    "./public/alpha-vue/**/*.js",
+    "./public/alpha-vue/**/*.html",
+  ],
   theme: {
     extend: {},
   },
